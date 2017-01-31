6effdc5509c38fd409252a33eb3996ed1c73b8df
Faisal Animashaun <d.f.animashaun@gmail.com>
2017-01-30 20:48:20 -0500

    completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function type(d) {
   d.speed = +d.Speed;
   d.spAttack = d['Sp. Atk'];
   d.spDefense = d['Sp. Def'];
+  d.type = d.Type;
 
   d.x = d.attack;
   d.y = d.defense;
@@ -18,5 +19,43 @@ d3.csv('pokemon.csv', type, function(error, data) {
         console.error(error)
         return;
     }
+    var dataset = new Plottable.Dataset(data);
 
+1   var xScale = new Plottable.Scales.Linear();
+    var yScale = new Plottable.Scales.Linear();
+
+    var xAxis = new Plottable.Axes.Numeric(xScale, 'bottom');
+    var yAxis = new Plottable.Axes.Numeric(yScale, 'left');
+
+    var plot = new Plottable.Plots.Scatter()
+        .x(function(d) { return d.x; }, xScale)
+        .y(function(d) { return d.y; }, yScale)
+        .addDataset(dataset);
+
+
+    var xInput = document.getElementById('x-input');
+    var yInput = document.getElementById('y-input');
+
+    xInput.addEventListener('change', function(event) {
+        var attribute = event.target.value;
+        dataset.data(dataset.data().map(function(d) {
+            d.x  = d[attribute];
+            return d;
+        }));
+    });
+
+    yInput.addEventListener('change', function(event) {
+        var attribute = event.target.value;
+        dataset.data(dataset.data().map(function(d) {
+            d.y  = d[attribute];
+            return d;
+        }));
+    });
+
+    var chart = new Plottable.Components.Table([
+        [yAxis, plot],
+        [null, xAxis]
+    ]);
+
+    chart.renderTo('svg#chart');
 });
